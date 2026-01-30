173ecdf96e83151fdb5fadcd4a132c0f2981635d
FerdawsAnzer <22301240@student.ciu.edu.tr>
2026-01-31 00:02:29 +0100

    more styling changes

--- src/components/DeskSetupSection.tsx
+++ src/components/DeskSetupSection.tsx
@@ -19,23 +19,46 @@ export function DeskSetupSection() {
 
   return (
     <section>
-      <Fade>
+      <Fade duration={1500} triggerOnce>
         <h2 className="fade-in">
           I’m a software engineering student passionate about building modern,
           user-focused web applications.
         </h2>
+      </Fade>
+
+      <Fade duration={1500} delay={300} triggerOnce>
+        {" "}
         <h2>
           I love creating interactive, intuitive interfaces that solve
           real-world problems.
         </h2>
-        <h2 className="fade-in" style={{ animationDelay: "0.3s" }}>
+      </Fade>
+
+      <Fade duration={1500} delay={600} triggerOnce>
+        <h2
+          className="fade-in"
+          style={{ animationDelay: "0.3s", marginTop: "0.5rem" }}
+        >
           This is my study desk — the space where I design interfaces, write
           code, and turn ideas into real projects.
         </h2>
       </Fade>
-      {desks.map((desk) => (
-        <DeskCard key={desk.id} desk={desk} />
-      ))}
+      <Fade duration={1500} delay={700} triggerOnce>
+        <h2
+          className="fade-in"
+          style={{ animationDelay: "0.3s", marginTop: "1rem" }}
+        >
+          <b>
+            Frontend-focused developer creating accessible and responsive web
+            experiences.
+          </b>
+        </h2>
+      </Fade>
+      <Fade duration={1500} delay={1000} triggerOnce>
+        {desks.map((desk) => (
+          <DeskCard key={desk.id} desk={desk} />
+        ))}
+      </Fade>
     </section>
   );
 }
